36c0d2854c071e6ae8f54aa835a01facc9d993d3
Reyhaneh Ghaffari <ghafarireyhaneh1@gmail.com>
2025-02-14 19:20:15 +0330

    create JS and HTML parts

---
+++ app.js
@@ -0,0 +1,31 @@
+const apiURL = "https://api.lyrics.ovh/v1";
+const lyricsEL = document.querySelector(".lyrics");
+
+const btnEL = document.querySelector(".findLyricsBtn");
+btnEL.addEventListener("click", async () => {
+  const artist = document.querySelector(".artistInput");
+  const song = document.querySelector(".songInput");
+
+  if (artist.value && song.value) {
+    try {
+      const response = await fetch(`${apiURL}/${artist.value}/${song.value}`);
+      const data = await response.json();
+
+      if (data.lyrics) {
+        displayLyrics(data.lyrics);
+      } else {
+        lyricsEL.innerText = "not found anything.";
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    alert("Please enter both artist and song title.");
+  }
+});
+
+function displayLyrics(lyrics) {
+  const verses = lyrics.split("\n");
+  const portion = verses.slice(0, 10);
+  lyricsEL.innerText = portion;
+}
